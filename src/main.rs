b2c153297f6f8e2004f494fb1e35c4091e28ac87
kri4er <mynamekaldery@gmail.com>
2024-03-08 23:15:26 +0100

    feat: basic lsp and tracing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,88 @@
-fn main() {
-    println!("Hello, world!");
+use tower_lsp::jsonrpc::{Result, Error};
+use tower_lsp::lsp_types::*;
+use tower_lsp::{Client, LanguageServer, LspService, Server};
+
+#[derive(Debug)]
+struct Backend {
+    client: Client,
+}
+
+#[tower_lsp::async_trait]
+impl LanguageServer for Backend {
+    async fn initialize(&self, _: InitializeParams) -> Result<InitializeResult> {
+        self.client
+            .log_message(MessageType::WARNING, "LOGME: AWS Code whisperer server initialized!")
+            .await;
+        Ok(InitializeResult {
+            server_info: None,
+            capabilities: ServerCapabilities {
+                inlay_hint_provider: Some(OneOf::Left(true)),
+                text_document_sync: Some(TextDocumentSyncCapability::Kind( TextDocumentSyncKind::FULL,)),
+                completion_provider: Some(CompletionOptions {
+                    resolve_provider: Some(false),
+                    trigger_characters: Some(vec![".".to_string()]),
+                    work_done_progress_options: Default::default(),
+                    all_commit_characters: None,
+                    completion_item: None,
+                }),
+                execute_command_provider: Some(ExecuteCommandOptions {
+                    commands: vec!["dummy.do_something".to_string()],
+                    work_done_progress_options: Default::default(),
+                }),
+                workspace: Some(WorkspaceServerCapabilities {
+                    workspace_folders: Some(WorkspaceFoldersServerCapabilities {
+                        supported: Some(true),
+                        change_notifications: Some(OneOf::Left(true)),
+                    }),
+                    file_operations: None,
+                }),
+                semantic_tokens_provider: None,
+                // definition: Some(GotoCapability::default()),
+                definition_provider: Some(OneOf::Left(true)),
+                references_provider: Some(OneOf::Left(true)),
+                rename_provider: Some(OneOf::Left(true)),
+                ..ServerCapabilities::default()
+            },
+        })
+    }
+
+    async fn did_save(&self, params: DidSaveTextDocumentParams) {
+        tracing::info!("Got a textDocument/didSave notification, here are params: {:?}", params);
+    }
+
+    async fn did_change(&self, params: DidChangeTextDocumentParams) {
+        tracing::info!("Got a textDocument/didChange notification, here are params: {:?}", params);
+    }
+
+    async fn completion(&self, params: CompletionParams) -> Result<Option<CompletionResponse>> {
+        tracing::error!("Got a textDocument/completion notification, with params: {:?}", params);
+        Err(Error::method_not_found())
+    }
+
+    async fn initialized(&self, _: InitializedParams) {
+        println!("LOGME: printAWS Code Whisperer is initialized now.");
+        self.client
+            .log_message(MessageType::WARNING, "LOGME: AWS Code whisperer server initialized!")
+            .await;
+    }
+
+    async fn shutdown(&self) -> Result<()> {
+        Ok(())
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let stdin = tokio::io::stdin();
+    let stdout = tokio::io::stdout();
+
+    let file_appender = tracing_appender::rolling::hourly("/tmp/debug_code_whisperer", "test1.log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+    tracing_subscriber::fmt()
+        .with_writer(non_blocking)
+        .init();
+
+
+    let (service, socket) = LspService::new(|client| Backend { client });
+    Server::new(stdin, stdout, socket).serve(service).await;
 }
